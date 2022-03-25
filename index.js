60a7b9425b7c28aa9513cdc413700a117e47b085
noumi92 <noumankarim1988@gmail.com>
2022-03-25 19:42:16 +0500

    Promises applied

--- index.js
+++ index.js
@@ -4,33 +4,30 @@ const dbDriver = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
+var doc = {name: "Paratha", description: "Delicious"};
+var coll = "dishes";
 
 
-MongoClient.connect(url, (err, client) => {
-
-    assert.equal(err,null);
-
-    console.log('Connected correctly to server');
-
-    const db = client.db(dbname);
-    var doc = {name: "Paratha", description: "Delicious"};
-    var coll = "dishes";
-
-    dbDriver.insertDocument(db, doc, coll, (result) => {
+MongoClient.connect(url)
+    .then((client) => {
+        console.log('Connected correctly to server');
+        const db = client.db(dbname);
+        return dbDriver.insertDocument(db, doc, coll)
+    .then((result) => {
         console.log("Insert Document:\n", result.ops);
-    });
-
-    dbDriver.findDocuments(db, coll, (doc)=>{
+        return dbDriver.findDocuments(db, coll)
+    }).then((doc)=>{
         console.log("Found Document:\n", doc);
-        dbDriver.updateDocument(db, {name: "Paratha"}, {description: "More Delicious"}, coll, (result)=>{
-            console.log("Updated document:\n", result.result);
-        });
-        dbDriver.findDocuments(db, coll, (doc)=>{
-            console.log("Found Document:\n", doc); 
-            db.dropCollection(coll, (result)=>{
-                console.log("Dropped collection:\n", result);
-                client.close();
-            });
-        });
+        return dbDriver.updateDocument(db, {name: "Paratha"}, {description: "More Delicious"}, coll)
+    }).then((result)=>{
+        console.log("Updated document:\n", result.result);
+        return dbDriver.findDocuments(db, coll)
+    }).then((doc)=>{
+        console.log("Found Document:\n", doc); 
+        return db.dropCollection(coll)
+    }).then((result)=>{
+        console.log("Dropped collection:\n", result);
+        client.close();
     });
-});
+})
+.catch((err)=> console.log(err));
--- operations.js
+++ operations.js
@@ -2,38 +2,20 @@ const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result)=>{
-        assert.equal(err, null);
-        console.log("inserted" + result.result.n +
-        " document into collection "+ collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs)=>{
-        assert.equal(err, null);
-        callback(docs);
-    })
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result)=>{
-        assert.equal(err, null);
-        console.log("Removed document ", document);
-        callback(result);
-    })
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, {$set:update}, null, (err, result)=>{
-        assert.equal(err, null);
-        console.log("Updated document with ", update);
-        callback(result);
-    })
+    return coll.updateOne(document, {$set:update}, null);
 };
-
-
